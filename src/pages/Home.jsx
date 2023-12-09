6b6e1f377683e56794c063aea9b331ebd4e5298e
Kenneth <kwadwobaahdanso@gmail.com>
2023-12-09 19:41:28 +0000

    updates on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import { getUser } from '../hooks/user.action'
+import { Row, Col, Image } from 'react-bootstrap';
+import { randomAvatar } from '../utils';
+import CreatePost from '../components/post/CreatePost';
+import { fetcher } from '../helpers/axios';
+import useSWR from 'swr'
+import Post from '../components/post/Post';
+
 
 function Home() {
+  const posts = useSWR("/post/", fetcher,{
+    refreshInterval:10000,
+  })
+  const user = getUser();
+
+  if(!user){
+    return <div>Loading</div>
+  }
+
   return (
-    <div>
-      <h1>Welcome page </h1>
-      <p>Profile</p>
-    </div>
+    <Layout>
+       <Row className='justify-content-evenly'>
+         <Col sm={7}>
+          <Row className='border rounded align-items-center'>
+            <Col>
+             <Image
+               src={randomAvatar()}
+               roundedCircle
+               width={52}
+               height={52}
+               />
+            </Col>
+
+            <Col sm={10} className='flex-grow-1'>
+            <CreatePost refresh={posts.mutate}/>
+            </Col>
+          </Row>
+
+          <Row className='my-4'>
+             {posts.data?.results.map((post,index)=>{
+              return(
+                <Post key={index} post={post} 
+                refresh={posts.mutate}/>
+              )
+             })}
+          </Row>
+         </Col>
+       </Row>
+    </Layout>
   )
 }
 
